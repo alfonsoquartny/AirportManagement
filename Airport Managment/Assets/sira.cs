2d7baeb3cfd37b8a682a97357cd6306e82e5dbca
alfonsoquartny <seyrelmeyen2@outlook.com>
2022-07-01 04:10:29 +0300

    fixed issues

--- Airport Managment/Assets/sira.cs
+++ Airport Managment/Assets/sira.cs
@@ -6,14 +6,21 @@ public class sira : MonoBehaviour
 {
 
     public GameObject[] siras;
+    public bool canSpawn;
+    public float timer;
+    private float defaultTimer;
+
+    public GameObject prefab;
     void Start()
     {
-        
+        defaultTimer = timer;
     }
 
     // Update is called once per frame
     void Update()
     {
+      
+
         if (siras[0].transform.name=="siraNull")
         {
             if (siras[0].transform.name == "siraNull")
@@ -24,7 +31,7 @@ public class sira : MonoBehaviour
                     siras[4] = siras[2];
                     if (siras[2].transform.name == "siraNull")
                     {
-                        siras[4] = siras[2];
+                        siras[4] = siras[3];
                         if (siras[3].transform.name == "siraNull")
                         {
                             siras[4] = null;
@@ -35,12 +42,29 @@ public class sira : MonoBehaviour
 
             }
 
-           
 
-           
 
-           
-          
+            if (siras[4] == null)
+            {
+                canSpawn = false;
+            }
+            else
+            {
+                canSpawn = true;
+
+            }
+            if (canSpawn == true)
+            {
+                timer -= Time.deltaTime;
+            }
+
+            if (timer < 0)
+            {
+                Instantiate(prefab);
+                timer = defaultTimer;
+            }
+
+
         }
 
         if (siras[0].transform.name == "sira0")
